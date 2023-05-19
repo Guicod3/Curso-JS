037f9ff3238dae06510c81a701e0292b0cee516c
Guicod3 <guilmiranda50@gmail.com>
2023-05-18 23:21:56 -0300

    estudy

---
+++ Aula 11/ex011.js
@@ -0,0 +1,25 @@
+var dia = new Date();
+var diasem = dia.getDay();
+
+switch (diasem) {
+  case 0:
+    console.log("Domingo");
+    break;
+  case 2:
+    console.log("Segunda");
+    break;
+  case 3:
+    console.log("Terça");
+    break;
+  case 4:
+    console.log("Quarta");
+    break;
+  case 5:
+    console.log("Quinta");
+    break;
+  case 6:
+    console.log("Sexta");
+    break;
+}
+
+console.log(diasem);
