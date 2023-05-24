ed51ed0a53f64fc3de9d0ffbeafc26addca0617f
Guicod3 <guilmiranda50@gmail.com>
2023-05-23 23:06:50 -0300

    Nada funciona

--- exercicios/parte1/script.js
+++ exercicios/parte1/script.js
@@ -1,10 +1,1 @@
-function carregar() {
-  var msg = document.getElementById("msg");
-  var img = document.getElementById("img");
-  var date = new date();
-  var hora = data.getHours();
-  msg.innerHTML = "Agora são " + hora + "horas.";
-}
 
-var hora = data.getHours();
-console.log(hora);
