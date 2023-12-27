473bb3c3b3e7a3812888b2f368d8b12995ace6cf
Guilherme Miranda <125209869+Guicod3@users.noreply.github.com>
2023-12-26 23:09:02 -0300

    Atualização de atributos

--- exercicios/parte2/script.js
+++ exercicios/parte2/script.js
@@ -1,10 +1,15 @@
-var tempo = new Date();
-var ano = tempo.getFullYear()
-var anodigitado = document.getElementById('anonasc');
-function verificar(){
-    var resultado = document.getElementById('resultado');
-    if (anodigitado.value.length == 0 || Number(anodigitado.value) >= ano){
-        resultado.innerHTML = `[Ano inválido] Por favor altere o ano acima`;
+function verificar(){    
+    var tempo = new Date()
+    var ano = tempo.getFullYear()
+    var iptano = document.getElementById('anonasc');
+    var res = document.getElementById('resultado');
+    if (iptano.value.length == 0 || Number(iptano.value) >= ano){
+        window.alert('[ERRO] Por favor, insira um ano válido.');
+    } else{
+        //var sex = document.getElementByname('radsex');
+        var idade = ano - Number(iptano.value);
+        res.innerHTML = `A idade correspondente é: ${idade} anos`;
     }
 }
+
 
