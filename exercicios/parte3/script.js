4ed87c2aa9f0df19a8ab0b7a0bcd2b1e96ede2de
Guilherme Miranda <125209869+Guicod3@users.noreply.github.com>
2024-01-03 22:08:31 -0300

    atualização ex

--- exercicios/parte3/script.js
+++ exercicios/parte3/script.js
@@ -3,7 +3,18 @@ function contar(){
     var final = document.getElementById('fim');
     var pulo = document.getElementById('pass');
     var resultado = document.getElementById('resultado');
-    for (var base; base <= final; base = base + Number(pulo.value)){
-        resultado.innerHTML = `${base}, `
+
+    if(base.value.length == 0 || final.value.length == 0 || pulo.value.length == 0){
+        window.alert("[ERRO] Insira valores válidos.")
+    } else{
+        let i = Number(base.value)
+        let f = Number(base.value)
+        let p = Number(base.value)
+
+        resultado.innerHTML = ('Contando: ')
+
+        for (let c = i; c <= f; c = c + p){
+            resultado.innerHTML += `${c}, `
+        } 
     }
 }
