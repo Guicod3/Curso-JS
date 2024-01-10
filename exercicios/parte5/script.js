d184f19fc4d210c629e9bd68c6c7a76872fc2fa8
Guilherme Miranda <125209869+Guicod3@users.noreply.github.com>
2024-01-09 21:25:04 -0300

    melhorado

--- exercicios/parte5/script.js
+++ exercicios/parte5/script.js
@@ -1,13 +1,16 @@
-function analisar(){
+function add(){
     let entrada1 = document.getElementById('inicio')
     let res = document.getElementById('resultado')
-    
+    let a = []
+   
     if (entrada1.value.length == 0){
         window.alert('[ERRO] Campo insert vazio')
         } else if (Number(entrada1.value) < 1 || Number(entrada1.value) > 100){
-            window.alert('[ERRO] Insira Valores de 1 a 100')
+            window.alert('[ERRO] Insira Valores de 1 a 100 não repetidos')
             } else{
-                let a = []
                 a.push(Number(entrada1.value))
+                let item = document.createElement('option');
+                item.text = `Valor ${a} adicionado.`
+                res.appendChild(item)
             }
 } 
