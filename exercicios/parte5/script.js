05ea86a8b6b8ba254ef264a20782d5768833d42a
Guilherme Miranda <125209869+Guicod3@users.noreply.github.com>
2024-01-10 23:12:27 -0300

    Tentativa de arrumar problema

--- exercicios/parte5/script.js
+++ exercicios/parte5/script.js
@@ -1,11 +1,13 @@
+
 function add(){
     let entrada1 = document.getElementById('inicio')
     let res = document.getElementById('resultado')
     let a = []
+    let c = -1
    
     if (entrada1.value.length == 0){
         window.alert('[ERRO] Campo insert vazio')
-        } else if (Number(entrada1.value) < 1 || Number(entrada1.value) > 100){
+        } else if (Number(entrada1.value) < 1 || Number(entrada1.value) > 100 || a.indexOf(Number(entrada1.value)) != -1){
             window.alert('[ERRO] Insira Valores de 1 a 100 não repetidos')
             } else{
                 a.push(Number(entrada1.value))
@@ -13,4 +15,4 @@ function add(){
                 item.text = `Valor ${a} adicionado.`
                 res.appendChild(item)
             }
-} 
+        }
