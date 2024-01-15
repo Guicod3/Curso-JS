954bcc4705f6d200e7ffa7156a85029d5faf4761
Guilherme Miranda <125209869+Guicod3@users.noreply.github.com>
2024-01-15 20:23:30 -0300

    Update script.js

--- exercicios/parte5/script.js
+++ exercicios/parte5/script.js
@@ -1,7 +1,3 @@
-let entrada1 = document.getElementById('inicio')
-let res = document.getElementById('resultado')
-let a = []
-
 function isnumber(n){
     if (n >= 1 && n <= 100){
         return true
@@ -10,17 +6,28 @@ function isnumber(n){
     }
 }
 
+function is_repete(n, f){
+    if (n.indexOf(Number(f)) != -1){
+        return true
+    } else{
+        return false
+    }
+}
+
+
+
 function add(){
-    let input_n = 
-    window.alert(`funfou ${Number(entrada1.value)}`)
-    /* if (isnumber(Number(entrada1.value)) && is_repete(a, Number(entrada1.value))){
+    let a = []
+    let res = document.getElementById('resultado')
+    let entrada1 = document.getElementById('inicio')
+    if (isnumber(Number(entrada1.value)) && !is_repete(a, entrada1.value)){
         a.push(Number(entrada1.value))
         let item = document.createElement('option');
         item.text = `Valor ${a} adicionado`
         res.appendChild(item)
     } else{
         window.alert('[ERRO] Insira Valores de 1 a 100 não repetidos')
-    }*/
+    }
 }
 
 
