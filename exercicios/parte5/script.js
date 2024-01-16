aa6727cdad12f1ff92090b05924a5bd58c419773
Guilherme Miranda <125209869+Guicod3@users.noreply.github.com>
2024-01-15 21:22:28 -0300

    exercicio nao finalizado

--- exercicios/parte5/script.js
+++ exercicios/parte5/script.js
@@ -1,33 +1,29 @@
-function isnumber(n){
-    if (n >= 1 && n <= 100){
-        return true
-    }else{
-        return false
-    }
-}
+let a = []
+let res = document.getElementById('resultado')
+let entrada1 = document.getElementById('inicio')
 
-function is_repete(n, f){
-    if (n.indexOf(Number(f)) != -1){
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else{
         return false
     }
 }
 
+function inLista(n, l) {
+    if (KeyboardEvent.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
+    }
+}
 
-
-function add(){
-    let a = []
-    let res = document.getElementById('resultado')
-    let entrada1 = document.getElementById('inicio')
-    if (isnumber(Number(entrada1.value)) && !is_repete(a, entrada1.value)){
+function adicionar() {
+    if(isNumero(entrada1.value) && !inLista(entrada1.value, a)){
         a.push(Number(entrada1.value))
-        let item = document.createElement('option');
-        item.text = `Valor ${a} adicionado`
-        res.appendChild(item)
-    } else{
-        window.alert('[ERRO] Insira Valores de 1 a 100 não repetidos')
+        let item = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+    }else{
+        window.alert('Valor inválido ou já encontrado na lista')
     }
 }
-
-
